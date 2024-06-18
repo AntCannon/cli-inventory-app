3a9440566249a548fa6888be893175f0e7ebfd9b
AntCannon <anthonycannonier@gmail.com>
2024-06-17 20:37:11 -0400

    update cart shape and cart total

--- utils/shopping.js
+++ utils/shopping.js
@@ -9,19 +9,28 @@ function centsToDollars(cents) {
 
 function addToCart(inventory, cart, itemName) {
   const item = getItemByName(inventory, itemName)
-  const cartItem = {
-    id: item.id,
-    item,
-    count: cart.find(({id}) => id === item.id).count + 1 || 1
+  
+  const itemCartIndex = cart.findIndex(({id}) => id === item.id)
+  console.log(itemCartIndex)
+
+  if (itemCartIndex > -1) {
+    cart[itemCartIndex].quantity = cart[itemCartIndex].quantity + 1
+    inform(`${itemName} quantity updated to ${cart[itemCartIndex].quantity}`)
+  } else {
+    const cartItem = {
+      id: item.id,
+      item,
+      quantity: 1
+    }
+    cart.push(cartItem)
+    inform(`${itemName} added to cart! Continue shopping.`)
   }
-  // update cart item count
-  cart.push(item)
-  inform(`${itemName} added to cart! Continue shopping.`)
+  
   return cart
 }
 
 function getCartTotal(cart) {
-  const total = cart.reduce((sum, {priceInCents}) => sum + +priceInCents, 0)
+  const total = cart.reduce((sum, cartItem ) => sum + (cartItem.item.priceInCents * cartItem.quantity), 0)
   return centsToDollars(total)
 }
 
